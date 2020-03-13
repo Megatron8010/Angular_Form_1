a6e3b5fe1bc12eea56831a87f27a1aedbb682d6e
Megatron8010 <aditya.singh801010@gmail.com>
2020-03-13 14:45:41 +0530

    Working addition functionality

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -8,6 +8,26 @@ import { EmployeeData } from '../employee-data.ts';
 })
 export class FormComponent implements OnInit {
   model = new EmployeeData(123,"Aditya",10000,"Sales");
+  headers = ["empId", "empName", "empSal", "empDep"];
+  rows =         [
+                   {empId:1001,empName:"Rahul",empSal:9000,empDep:"Java"},
+                   {empId:1002,empName:"Sachin",empSal:19000,empDep:"OraApps"},
+                   {empId:1003,empName:"Vikash",empSal:29000,empDep:"BI"},
+                   ];
+  onUpdate(formObj) {
+    let values = formObj.value;
+    console.log(values);
+    let obj: any = {
+      empId: values.id,
+      empName: values.name,
+      empSal: values.salary,
+      empDep: values.department
+    };
+
+    this.rows.push(obj);
+    formObj.reset();
+
+  }
   constructor() { }
 
   ngOnInit() {
